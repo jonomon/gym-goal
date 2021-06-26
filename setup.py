3fcae3767d77c6788dbd6bff87bc42975262a009
Jonathan Chung <jono.chung@gmail.com>
2021-06-26 13:50:33 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='gym_goal',
     version='0.0.1',
     description='Goal domain OpenAI Gym environment',
     author='Craig James Bester',
-    packages=['gym_goal'],
+    packages=find_packages(include=['gym_goal', 'gym_goal.*']),
     install_requires=['gym',
                       'pygame', #'pygame>=1.9.3'
                       'numpy',  #'numpy>=1.14.0'
